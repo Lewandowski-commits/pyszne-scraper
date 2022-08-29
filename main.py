02269261a02a1661fe57a9b5ca0a97be3af9a18c
Lewandowski-commits <michalewandowski0@gmail.com>
2022-08-29 15:24:30 +0200

    remove unused import

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import re
 
 BASE_URL = 'https://www.pyszne.pl/'
 RESTAURANT_LIST_SUFFIX = 'na-dowoz/'
