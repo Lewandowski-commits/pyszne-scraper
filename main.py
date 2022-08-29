c8d9fee176d5e6575f1b3f97afd3e44978df107d
Lewandowski-commits <michalewandowski0@gmail.com>
2022-08-29 13:27:11 +0200

    add chrome options for docker

--- main.py
+++ main.py
@@ -3,15 +3,29 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import re
 
 BASE_URL = 'https://www.pyszne.pl/'
 RESTAURANT_LIST_SUFFIX = 'na-dowoz/'
 
+def set_chrome_options() -> None:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
+
 def main(BASE_URL: str = BASE_URL, RESTAURANT_LIST_SUFFIX: str = RESTAURANT_LIST_SUFFIX, search_string: str = 'poznan-poznan-grunwald-61-801?sortBy=name'):
     service = Service(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=set_chrome_options())
     driver.get('https://www.pyszne.pl/na-dowoz/jedzenie/poznan-poznan-grunwald-61-801?sortBy=name')
     restaurant_count = WebDriverWait(driver, timeout=3).until(lambda x: x.find_element(By.XPATH, "//*[@id='page']/div[4]/section/div[1]/div/div[2]/div/h1"))
     restaurant_count = int(re.findall('[0-9]+', restaurant_count.text)[0])
